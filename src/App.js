ff032203fa0f231de61cd292a58e910a94896680
andori8 <falcsmike7@gmail.com>
2019-02-06 08:48:34 -0500

    installed tachyons and created navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navigation from './components/navigation/Navigation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Navigation />
       </div>
     );
   }
