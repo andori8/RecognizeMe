faa64ae62e60863cae7c783431f146e2305b2774
andori8 <falcsmike7@gmail.com>
2019-02-06 09:47:05 -0500

    added searchbox and rank component and installed particles.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import Rank from './components/rank/Rank';
+import SearchBox from './components/SearchBox/SearchBox';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions}/>
         <Navigation />
         <Logo />
+        <Rank />
+        <SearchBox />
       </div>
     );
   }
